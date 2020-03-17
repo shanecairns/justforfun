840eca7e261667e72cd134ae0af30daac0eebd66
shanecairns <35631139+shanecairns@users.noreply.github.com>
2020-03-17 16:45:06 -0400

    Create helper.h

---
+++ helper.h
@@ -0,0 +1,17 @@
+
+#ifndef HELPER_H
+#define HELPER_H
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int printMenu(string menu);
+
+int validateChoice(int choice, string menu);
+
+
+#include "helper.hpp"
+#endif
